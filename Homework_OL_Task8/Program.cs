a008b85bc49f0ee6850ef3e8db49f270bfced5fb
ThinksSo <stpt.mail@yandex.ru>
2022-09-16 12:38:53 +0300

    add Task8 solution

---
+++ Homework_OL_Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine();
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int div = num % 2;
+int count = 2;
+
+Console.Write($"{num} -> ");
+if (num < 2) Console.Write("value less than 2");
+if (div != 0) num = num - 1;
+if (num >= 2) Console.Write(count);
+
+while (num > count)
+{
+    count += 2;
+    Console.Write($", {count}");
+}
+Console.WriteLine("\n");
