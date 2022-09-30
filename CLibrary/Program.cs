2fe4478c11e0637e1ddb792758500f33e66a1879
ThinksSo <stpt.mail@yandex.ru>
2022-09-30 12:56:24 +0300

    add C# library: Array filling, sorting, print

---
+++ CLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CLib
+{
+    class Array
+    {
+        // Array filling - Заполнение массива
+        public static void FillArray(int[] numbers,
+                                    int minValue = 0,
+                                    int maxValue = 100)
+        {
+            maxValue++;
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue);
+            }
+        }
+
+        // Array sorting - сортировка массива (пузырьковая)
+        public static void SortArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+
+            }
+        }
+
+
+        // Array print - вывод массива в консоль
+        public static void PrintArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
---
+++ CLibrary/TestLib.cs
@@ -0,0 +1,15 @@
+static void Test4Lib()
+{
+    int size = 10;
+    int[] newArr = new int [size];
+    int minArg = -10;
+    int maxArg = 10;
+
+    CLib.Array.FillArray(newArr, minArg, maxArg);
+    CLib.Array.PrintArray(newArr);
+    CLib.Array.SortArray(newArr);
+    CLib.Array.PrintArray(newArr);
+    Console.WriteLine();
+}
+
+Test4Lib();
