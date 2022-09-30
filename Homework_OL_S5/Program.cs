1e6ee727534f65be9b04f2c68b108baf0be74db7
ThinksSo <stpt.mail@yandex.ru>
2022-09-30 19:54:14 +0300

    Task38: Array max - min

--- Homework_OL_S5/Program.cs
+++ Homework_OL_S5/Program.cs
@@ -7,12 +7,12 @@ void Task34()
 {
     Console.WriteLine("\n \t Task 34");
     Random random = new Random();
-    int size = random.Next(2, 15);    
-    
+    int size = random.Next(2, 15);
+
     int[] numbers = new int[size];
     int minArg = 100, maxArg = 999;
-    
-    CLib.Array.FillArray(numbers, minArg, maxArg); 
+
+    CLib.Array.FillArray(numbers, minArg, maxArg);
 
     int count = 0;
     for (int i = 0; i < numbers.Length; i++)
@@ -20,7 +20,7 @@ void Task34()
         if (numbers[i] % 2 == 0) count++;
     }
     CLib.Array.PrintArray(numbers);
-    Console.WriteLine("-> " + count);
+    Console.WriteLine("Even numbers -> " + count);
 }
 
 /* Задача 36: 
@@ -33,25 +33,61 @@ void Task36()
 {
     Console.WriteLine("\n \t Task 36");
     Random random = new Random();
-    int size = random.Next(2, 15);    
+    int size = random.Next(2, 15);
     int[] numbers = new int[size];
     int minArg = -100, maxArg = 100;
-    
-    CLib.Array.FillArray(numbers, minArg, maxArg); 
+
+    CLib.Array.FillArray(numbers, minArg, maxArg);
 
     int sum = 0;
-    for (int i = 1; i < numbers.Length; i+=2)
+    for (int i = 1; i < numbers.Length; i += 2)
     {
         sum = sum + numbers[i];
     }
-    
+
     CLib.Array.PrintArray(numbers);
-    Console.WriteLine("sum -> " + sum);
+    Console.WriteLine("Sum with odd index -> " + sum);
     Console.WriteLine();
+}
+
+/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+void Task38()
+{
+    Console.WriteLine("\n \t Task 38");
+
+    Random random = new Random();
+    int size = random.Next(2, 10);
+    double[] numbers = new double[size];
+    
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers [i] = Math.Round(random.Next(-10, 100) + random.NextDouble(), 2);
+        Console.Write(numbers[i] + " ");
+    }
+    
+    //for (int i = 0; i < numbers.Length; i++) Console.Write(numbers[i] + " ");
+    
+    double max = numbers[0];
+    double min = numbers[0];
+    for (int j = 0; j < numbers.Length-1; j++) 
+    {
+        if (numbers[j] < numbers[j+1]) max = numbers[j+1];
+        if (numbers[j] > numbers[j+1]) min = numbers[j+1];
+    }
+    Console.WriteLine($"\n Min - Max = {max} - {min} = {Math.Round(max - min, 2)}");
+
+    Console.WriteLine();
+
 }
 
 
 
 Task34();
 Task36();
+Task38();
+
 
